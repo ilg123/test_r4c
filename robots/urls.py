ddee9efb5181358f0e37276bce022cc08a4f9f31
Ilgizar Kachaev <mcilg123@gmail.com>
2024-12-14 20:57:35 +0300

    Task 2 completed/reworked validation

--- robots/urls.py
+++ robots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_robot
+from .views import create_robot, generate_excel_report
 
 urlpatterns = [
     path('create_robot/', create_robot, name='create_robot'),
+    path('create_report/', generate_excel_report, name='create_report'),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,26 +1,31 @@
 import json
-from django.http import JsonResponse
+import datetime
+from openpyxl import Workbook
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Robot
 from django.utils.dateparse import parse_datetime
+from django.db.models import Count
 
 @csrf_exempt
 def create_robot(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
+            model = data['model']
+            version = data['version']
+            created = parse_datetime(data['created'])
 
-            production_date = parse_datetime(data['created'])
-            if production_date is None:
-                return JsonResponse({'Ошибка': 'Не верная дата'},)
-            
-            try:
-                robot_model = Robot.objects.get(serial = data['serial'], model=data['model'], version=data['version'])
-                robot_version = Robot.objects.get(version=data['version'])
-            except Robot.DoesNotExist as e:
-                return JsonResponse({'Ошибка': str(e)}, status=400)
+            if not model or not version or not created:
+                return JsonResponse({'Ошибка': 'Не верные данные.'}, status=400)
 
-            robot = Robot(model = robot_model, version = robot_version,  created = production_date)
+            if not Robot.objects.filter(model=model).exists():
+                return JsonResponse({'Ошибка': f'Модели {model} нет в базе.'}, status=400)
+
+            if not Robot.objects.filter(model=model, version=version).exists():
+                return JsonResponse({'Ошибка': f'Версии {version} для модели {model} нет в базе.'}, status=400)
+
+            robot = Robot.objects.create(model=model, version=version, created=created)
             robot.save()
             return JsonResponse({'Сообщение': 'Запись создана успешно'}, status=201)
 
@@ -28,4 +33,43 @@ def create_robot(request):
             return JsonResponse({'Ошибка': str(e)}, status=400)
 
     else:
-        return JsonResponse({'Ошибка': 'Доступен только POST-method'}, status=400)
+        return JsonResponse({'Ошибка': 'Доступен только POST-method'}, status=405)
+
+
+def generate_excel_report(request):
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(days=7)
+
+    robots = Robot.objects.filter(created__range=(start_date, end_date))
+
+    wb = Workbook()
+
+    robot_models = robots.values('model').distinct()
+
+    sheet_created = False
+
+    for robot_model in robot_models:
+        model_name = robot_model['model']
+        ws = wb.create_sheet(title=model_name)
+        ws.append(['Модель', 'Версия', 'Количество за неделю'])
+
+        model_robots = robots.filter(model=model_name)
+        versions = model_robots.values('version').annotate(total=Count('id'))
+
+        for version in versions:
+            ws.append([model_name, version['version'], version['total']])
+        
+        sheet_created = True
+
+    if 'Sheet' in wb.sheetnames and sheet_created:
+        del wb['Sheet']
+
+    if not sheet_created:
+        ws = wb.active
+        ws.title = "No Data"
+        ws.append(['Нет данных за последнюю неделю'])
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="robots_report.xlsx"'
+    wb.save(response)
+    return response
